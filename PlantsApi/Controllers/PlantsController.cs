0f3ed0d004cf125f2ab7f0911f80797aa0820c3a
TocinoandLessie <sandragfrontan87@gmail.com>
2021-07-23 12:25:21 +0100

    adding initial code

---
+++ PlantsApi/Controllers/PlantsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlantsController : ControllerBase
+    {
+        private static readonly string[] Plants = new[]
+        {
+            "Monstera", "Banana", "Tits"
+        };
+        private readonly ILogger<PlantsController> _logger;
+
+        public PlantsController(ILogger<PlantsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]//.../Plants
+        public IEnumerable<string> Get()
+        {
+            return Plants;
+        }
+    }
+}
